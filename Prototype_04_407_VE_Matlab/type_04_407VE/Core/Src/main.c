aec9541a3f759a9aa5367151736a015481c503e1
Polarislm <526788518@qq.com>
2024-06-05 23:16:29 +0800

    0605v2

--- Prototype_04_407_VE_Matlab/type_04_407VE/Core/Src/main.c
+++ Prototype_04_407_VE_Matlab/type_04_407VE/Core/Src/main.c
@@ -305,6 +305,7 @@ int Rcv_DealData(void){
 		}else if(0xBB==rcv_buf[0]&&0xCC==rcv_buf[7]){
 			HAL_GPIO_WritePin(RST_CTRL_GPIO_Port,RST_CTRL_Pin,GPIO_PIN_SET);
 			HAL_GPIO_WritePin(RST_CTRL_GPIO_Port,RST_CTRL_Pin,GPIO_PIN_RESET);
+			DATARELOAD(rcv_buf);
 		}
 		for(int i=0;i<8;i++){
 			rcv_buf[i]=0;
